342e0878eb30a151728abbaf1481c2c7802ad95c
Leticia Sandoval <letysan2932@gmail.com>
2017-09-06 13:43:15 -0500

    Se agreo parte de diseño para la vista formulario libro

--- application/views/formularios_libro/consultar_libro.php
+++ application/views/formularios_libro/consultar_libro.php
@@ -21,62 +21,62 @@
          </div>
 
 			<div class="form-group" col-lg-10>
-				<label for="Título del Articulo">Título del Articulo</label>
+				<label for="Título del Articulo" class="col-lg-4">Título del Articulo</label>
 				<input type="text" name="titulo_articulo" class="form-inline">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Autores</label>
+				<label class="col-lg-4">Autores</label>
 				<input type="text" name="autores" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Autores externos</label>
+				<label class="col-lg-4">Autores externos</label>
 				<input type="text" name="autoresExternos" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Título del Libro</label>
+				<label class="col-lg-4">Título del Libro</label>
 				<input type="text" name="título_del_libro" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Número del Capítulo</label>
+				<label class="col-lg-4">Número del Capítulo</label>
 				<input type="text" name="núm_del_capítulo" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Editores</label> 
+				<label class="col-lg-4">Editores</label> 
 				<input type="text" name="editores" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Editorial</label>
+				<label class="col-lg-4">Editorial</label>
 				<input type="text" name="editorial" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>ISBN</label>
+				<label class="col-lg-4">ISBN</label>
 				<input type="text" name="ISBN" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Año</label>
+				<label class="col-lg-4">Año</label>
 				<input type="text" name="año" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>País</label>
+				<label class="col-lg-4">País</label>
 				<input type="text" name="país" class="form-inline" required="true">
 			</div>
 			
 			<div class="form-group" col-lg-8>
-				<label>Página Inicio</label>
+				<label class="col-lg-4">Página Inicio</label>
 				<input type="text" name="pág_ini" class="form-inline" required="true">
 			</div>
 
 			<div class="form-group" col-lg-8>
-				<label>Página Fin</label>
+				<label class="col-lg-4">Página Fin</label>
 				<input type="text" name="pág_fin" class="form-inline" required="true">
 			</div>
 
