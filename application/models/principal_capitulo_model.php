e9bd1069781ff504b6ca7d97957d864451b194a2
Cecilia Jimenez <cecyjima@gmail.com>
2018-03-13 21:39:52 -0600

    se agregaron algunos comentarios

--- application/models/principal_capitulo_model.php
+++ application/models/principal_capitulo_model.php
@@ -6,21 +6,23 @@ class Principal_Capitulo_Model extends CI_Model
 	{
 		parent::__construct();
 	}
-
+	//Consultar la informacion de los registros
 	public function getCapitulos(){
 		return $this->db->query("SELECT * FROM capitulo_libro")->result();
 	}
+	//Agregar un nuevo registro de capitulo de libro
 	public function setCapitulo(string $titulo_capitulo, string $autores, string $autoresExternos, string $tipo, string $estatus, string $título_del_libro, int $núm_del_capítulo, string $editores, string $editorial, int $ISBN, int $año, string $país, int $paginas, string $URL){
 		return $this->db->query(" INSERT INTO capitulo_libro (titulo_capitulo, autores, autoresExternos, tipo, estatus, título_del_libro, núm_del_capítulo, editores, editorial, ISBN, año, país, paginas, URL) values ( {$titulo_capitulo},{$autores},{$autoresExternos},{$tipo},{$estatus},{$título_del_libro},{$núm_del_capítulo},{$editores},{$editorial},{$ISBN},{$año},{$país},{$paginas},{$URL}) " );
 	}
+	//
 	public function getCapitulo(int $id_capituloLibro){
 		return $this->db->query("SELECT id_capituloLibro, titulo_capitulo, autores, autoresExternos, tipo, estatus,título_del_libro, núm_del_capítulo, editores, editorial, ISBN, año, país, paginas, URL FROM capitulo_libro WHERE id_capituloLibro = {$id_capituloLibro}")->row();
 	}
-
+	//Editar la informacion de un registro de un capitulo de libro
 	public function actualizarCapitulo(int $id_capituloLibro, string $titulo_capitulo, string $autores, string $autoresExternos, string $tipo, string $estatus, string $título_del_libro, int $núm_del_capítulo, string $editores, string $editorial, int $ISBN, int $año, string $país, int $paginas, string $URL){
 		return $this->db->query("UPDATE capitulo_libro SET titulo_capitulo={$titulo_capitulo}, autores={$autores}, autoresExternos={$autoresExternos}, tipo={$tipo}, estatus={$estatus},título_del_libro={$título_del_libro}, núm_del_capítulo={$núm_del_capítulo}, editores={$editores}, editorial={$editorial}, ISBN={$ISBN}, año={$año}, país={$país}, paginas={$paginas}, URL={$URL} WHERE id_capituloLibro={$id_capituloLibro}");
 	}
-
+	//Eliminar un registro de capitulo de libro
 	public function eliminarCapitulo(int $id_capituloLibro){
 		return $this->db->query("DELETE FROM capitulo_libro WHERE id_capituloLibro = {$id_capituloLibro}");
 
--- application/models/principal_libro_model.php
+++ application/models/principal_libro_model.php
@@ -7,20 +7,22 @@ class Principal_Libro_Model extends CI_Model
 		parent::__construct();
 	}
 
+	//Consulta de libros registrados
 	public function getLibros(){
 		return $this->db->query("SELECT * FROM libro")->result();
 	}
+	//Registro de un nuevo libro
 	public function setLibro(string $título_del_libro, string $autores, string $autoresExternos, string $tipo, string $estatus, string $editorial, string $editores, int $ISBN, int $año, string $país, int $páginas, string $URL){
 		return $this->db->query(" INSERT INTO libro (título_del_libro, autores, autoresExternos, tipo, estatus, editorial, editores, ISBN, año, país, páginas, URL) values ( {$título_del_libro},{$autores},{$autoresExternos},{$tipo},{$estatus},{$editorial},{$editores},{$ISBN},{$año},{$país},{$páginas},{$URL}) " );
 	}
 	public function getLibro(int $id_libro){
 		return $this->db->query("SELECT id_libro, título_del_libro, autores, autoresExternos, tipo, estatus, editorial, editores, ISBN, año, país, páginas, URL FROM libro WHERE id_libro = {$id_libro}")->row();
 	}
-
+	//Editar la informacion de un registro
 	public function actualizarLibro(int $id_libro, string $título_del_libro, string $autores, string $autoresExternos, string $tipo, string $estatus, string $editorial, string $editores, int $ISBN, int $año, string $país, int $páginas, string $URL){
 		return $this->db->query("UPDATE libro SET título_del_libro={$título_del_libro}, autores={$autores},autoresExternos={$autoresExternos}, tipo={$tipo}, estatus={$estatus}, editorial={$editorial}, editores={$editores}, ISBN={$ISBN}, año={$año}, país={$país}, páginas={$páginas}, URL={$URL} WHERE id_libro={$id_libro}");
 	}
-
+	//Eliminar un registro
 	public function eliminarLibro(int $id_libro){
 		return $this->db->query("DELETE FROM libro WHERE id_libro = {$id_libro}");
 
